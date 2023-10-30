2ab14dae6dfa2c156c3bd84feacb6877a07f138a
root <root@QL.MikroTik>
2023-10-30 22:52:55 +0800

    修改日志打印逻辑，在默认情况下避免过多调试信息输出

--- gfa.go
+++ gfa.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -13,26 +14,33 @@ import (
 	"time"
 )
 
-var logger *log.Logger
-var authToken string
+var (
+	authToken   string
+	infoLogger  = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+	warnLogger  = log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime)
+	errorLogger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	debugLogger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+)
 
 // 处理上传文件请求逻辑
 func uploadFile(w http.ResponseWriter, r *http.Request, dirPath string) {
-	
-	// 输出请求方法和其他信息到控制台
-	fmt.Printf("请求方法：%s\n", r.Method)
-	fmt.Printf("请求URL：%s\n", r.URL.String())
-	fmt.Printf("请求客户端IP：%s\n", r.RemoteAddr)
+
+	// 输出请求方法和其他调试信息到控制台
+	debugLogger.Printf("请求客户端IP：%s\n", r.RemoteAddr)
+	debugLogger.Printf("请求方法：%s\n", r.Method)
+	debugLogger.Printf("请求URL：%s\n", r.URL.String())
 
 	// 获取口令参数
 	auth := r.FormValue("auth")
-	logger.Printf("口令参数：%s\n", auth)
 
 	// 口令验证逻辑
 	if auth != authToken {
 		// 口令认证失败，返回 401 错误代码
-		logger.Println("口令认证失败")
-		http.Error(w, "401 Unauthorized, 口令验证失败，经授权的访问", http.StatusUnauthorized)
+		warnLogger.Print("口令认证失败")
+		warnLogger.Printf("客户端IP：%s\n", r.RemoteAddr)
+		warnLogger.Printf("请求URL：%s\n", r.URL.String())
+		warnLogger.Print("口令参数：", auth)
+		http.Error(w, "401 Unauthorized, 口令验证失败，未经授权的访问", http.StatusUnauthorized)
 		return
 	}
 
@@ -56,73 +64,71 @@ func uploadFile(w http.ResponseWriter, r *http.Request, dirPath string) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 
-		// 输出程序运行日志
-		logger.Println(response)
+		// 输出请求日志
+		warnLogger.Print(response)
 		return
 	}
 
 	// 获取上传的文件
 	file, handler, err := r.FormFile("file")
 	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
+		errorLogger.Print("获取文件失败：", err)
 		return
 	}
 	defer file.Close()
 
 	// 获取当前所在目录参数
 	dir := r.FormValue("dir")
-	fmt.Println("dir:", dir)
+	debugLogger.Printf("原始路径： %s", dir)
 
 	// 解码目录参数
 	decodedDir, err := url.QueryUnescape(dir)
 	if err != nil {
 		// 处理解码错误
-		fmt.Println("URL decoding error:", err)
+		errorLogger.Print("获取文件目录出错：", err)
 		return
 	}
 
 	// 构建目标路径
 	dstPath := dirPath + decodedDir + "/" + handler.Filename
-	fmt.Println("dstPath:", dstPath)
+	debugLogger.Printf("本地路径：%s", dstPath)
 
 	// 创建保存文件的目标路径
 	dst, err := os.Create(dstPath)
 	if err != nil {
-		fmt.Println(err)
+		errorLogger.Print("目录创建失败：", err)
 		return
 	}
 	defer dst.Close()
 
 	// 将上传的文件拷贝到目标路径
 	if _, err := io.Copy(dst, file); err != nil {
-		fmt.Println("Copy error:", err)
+		errorLogger.Print("文件复制出错：", err)
 		return
 	}
 
-	// 返回上传成功的消息
-	fmt.Fprintf(w, "文件上传成功")
-
-	// 输出程序运行日志
-	logger.Printf("文件上传成功，目标路径：%s\n", dstPath)
+	// 输出文件保存成功信息和路径
+	infoLogger.Print("文件上传成功，目标路径：", dstPath)
 }
 
 // 处理删除请求逻辑
 func deleteFile(w http.ResponseWriter, r *http.Request, dirPath string) {
-	
-	// 输出请求方法和其他信息到控制台
-	fmt.Printf("请求方法：%s\n", r.Method)
-	fmt.Printf("请求URL：%s\n", r.URL.String())
-	fmt.Printf("请求客户端IP：%s\n", r.RemoteAddr)
+
+	// 输出请求方法和其他调试信息到控制台
+	debugLogger.Printf("请求客户端IP：%s\n", r.RemoteAddr)
+	debugLogger.Printf("请求方法：%s\n", r.Method)
+	debugLogger.Printf("请求URL：%s\n", r.URL.String())
 
 	// 获取口令参数
 	auth := r.FormValue("auth")
-	logger.Printf("口令参数：%s\n", auth)
-	
+
 	// 口令验证逻辑
 	if auth != authToken {
 		// 口令认证失败，返回 401 错误代码
-		logger.Println("口令认证失败")
+		warnLogger.Print("口令认证失败")
+		warnLogger.Printf("客户端IP：%s\n", r.RemoteAddr)
+		warnLogger.Printf("请求URL：%s\n", r.URL.String())
+		warnLogger.Print("口令参数：", auth)
 		http.Error(w, "401 Unauthorized, 口令验证失败，经授权的访问", http.StatusUnauthorized)
 		return
 	}
@@ -147,67 +153,85 @@ func deleteFile(w http.ResponseWriter, r *http.Request, dirPath string) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 
-		// 输出程序运行日志
-		logger.Println(response)
+		// 输出请求日志
+		warnLogger.Print(response)
 		return
 	}
 
 	// 获取要删除的文件路径参数
 	filePath := r.FormValue("file")
-	fmt.Println("filePath:", filePath)
+	debugLogger.Printf("原始路径： %s", filePath)
 
 	// 解码文件路径参数
 	decodedFilePath, err := url.QueryUnescape(filePath)
 	if err != nil {
 		// 处理解码错误
-		fmt.Println("URL decoding error:", err)
+		errorLogger.Print("获取文件目录出错：", err)
 		return
 	}
 
 	// 构建完整的文件路径
 	fullPath := dirPath + decodedFilePath
-	fmt.Println("fullPath:", fullPath)
+	debugLogger.Printf("本地路径：%s", fullPath)
 
 	// 删除文件
 	err = os.Remove(fullPath)
 	if err != nil {
 		// 处理删除错误
-		fmt.Println("File deletion error:", err)
+		errorLogger.Print("文件删除失败：", err)
 		return
 	}
 
-	// 返回删除成功的消息
-	fmt.Fprintf(w, "文件删除成功")
-
-	// 输出程序运行日志
-	logger.Printf("文件删除成功，文件路径：%s\n", fullPath)
+	// 输出删除成功信息和路径
+	infoLogger.Print("文件删除成功，文件路径：", fullPath)
 }
 
 func main() {
-	
+
 	// 解析命令行参数
 	port := flag.String("p", "8080", "设置服务器监听的端口号")
-	logLevel := flag.String("log", "info", "设置日志输出级别")
-	auth := flag.String("a", "", "设置口令认证字符串")
+	logLevel := flag.String("log", "info", "设置日志输出级别，支持：debug，info，warn，error")
+	auth := flag.String("a", "", "设置口令认证字符串（4-16位字符串，仅支持数字，大小写字母和英文符号）")
 	dir := flag.String("dir", ".", "设置网站根目录")
 	flag.Parse()
 
-	// 根据命令行参数初始化日志输出级别
+	// 根据命令行参数选择要使用的日志记录器
+	var selectedLogger *log.Logger
 	switch *logLevel {
 	case "debug":
-		logger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+		selectedLogger = debugLogger
 	case "info":
-		logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+		selectedLogger = infoLogger
 	case "warn":
-		logger = log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime)
+		selectedLogger = warnLogger
 	case "error":
-		logger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+		selectedLogger = errorLogger
 	default:
-		logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+		selectedLogger = infoLogger
+	}
+
+	// 非debug级别日志时不输出调试信息
+	if *logLevel != "debug" {
+		debugLogger.SetOutput(io.Discard)
 	}
 
 	// 设置日志输出到控制台
-	logger.SetOutput(os.Stdout)
+	selectedLogger.SetOutput(os.Stdout)
+
+	// 用户输入日志级别参数校验逻辑
+	validLogLevels := []string{"debug", "info", "warn", "error"}
+	isValidLogLevel := false
+	for _, level := range validLogLevels {
+		if *logLevel == level {
+			isValidLogLevel = true
+			break
+		}
+	}
+	if !isValidLogLevel {
+		fmt.Println("日志级别参数无效，请输入debug、info、warn或error")
+		flag.PrintDefaults()
+		return
+	}
 
 	// 口令验证变量以及处理逻辑
 	if *auth != "" {
@@ -223,13 +247,24 @@ func main() {
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		uploadFile(w, r, *dir)
 	})
+
 	// 设置/delete路由的处理函数为deleteFile函数
 	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
 		deleteFile(w, r, *dir)
 	})
 
+	// 端口可用性验证逻辑
+	listener, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		fmt.Printf("服务启动失败：%s\n", err)
+		return
+	}
+	listener.Close()
+
+	// 在启动前打印服务监听信息，避免执行http.ListenAndServe语句后造成阻塞无法打印
+	fmt.Printf("服务已启动，监听端口：%s\n", *port)
+
 	// 启动服务器，监听指定的端口
 	http.ListenAndServe(":"+*port, nil)
-	fmt.Printf("服务器已启动，监听端口：%s\n", *port)
 
 }
